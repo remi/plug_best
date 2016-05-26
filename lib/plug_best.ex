11d8bc38d55a9da581a1029b8cabb3372cb7955c
Rémi Prévost <remi@exomel.com>
2016-05-26 18:43:48 -0400

    Add code documentation

--- lib/plug_best.ex
+++ lib/plug_best.ex
@@ -1,4 +1,30 @@
 defmodule PlugBest do
+  @moduledoc """
+  A library that parses HTTP `Accept-*` headers and returns the best match based
+  on a list of values.
+
+  ## Examples
+
+  ```elixir
+  iex> conn = %Plug.Conn{req_headers: [{"accept-language", "fr-CA,fr;q=0.8,en;q=0.6,en-US;q=0.4"}]}
+  iex> conn |> PlugBest.best_language(["en", "fr"])
+  {"fr-CA", "fr", 1.0}
+
+  iex> conn = %Plug.Conn{req_headers: [{"accept-language", "es"}]}
+  iex> conn |> PlugBest.best_language(["fr", "ru"])
+  nil
+
+  iex> conn = %Plug.Conn{req_headers: [{"accept-language", "es"}]}
+  iex> conn |> PlugBest.best_language_or_first(["ru", "fr"])
+  {"ru", "ru", 0.0}
+
+  ```
+  """
+
+  @doc """
+  Returns the best supported langage based on the connection `Accept-Language`
+  HTTP header. Returns `nil` if none is found.
+  """
   def best_language(conn = %Plug.Conn{}, supported_languages) do
     # Fetch the raw header content
     conn |> fetch_header_value("accept-language")
@@ -17,6 +43,10 @@ defmodule PlugBest do
     |> List.first
   end
 
+  @doc """
+  Returns the best supported langage based on the connection `Accept-Language`
+  HTTP header. Returns the first supported language if none is found.
+  """
   def best_language_or_first(conn = %Plug.Conn{}, supported_languages) do
     conn |> best_language(supported_languages) || default_supported_language(supported_languages)
   end
--- test/plug_best_test.exs
+++ test/plug_best_test.exs
@@ -1,6 +1,7 @@
 defmodule PlugBestTest do
   use ExUnit.Case, async: true
   use Plug.Test
+  doctest PlugBest
 
   test "returns the best language" do
     conn = %Plug.Conn{req_headers: [{"accept-language", "fr-CA,fr;q=0.8,en;q=0.6,en-US;q=0.4"}]}
