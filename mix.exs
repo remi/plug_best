16ac20bf22873eabedbc311d8dfe0f4154e6d451
Rémi Prévost <remi@exomel.com>
2017-02-13 06:07:36 -0500

    Cache dialyzer PLTs (#8)

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule PlugBest.Mixfile do
   end
 
   def dialyzer do
-    [plt_add_apps: [:plug]]
+    [plt_add_apps: [:plug],
+     plt_file: ".plts/.local.plt",
+     plt_core_path: ".plts"]
   end
 
   defp deps do
